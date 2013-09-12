05bf14272dd51ae52480fb9f2318f5fbe591d5ff
Gustavo Lopes <gustavo@thehyve.nl>
2013-09-12 15:21:49 +0200

    Allow loader.sh to be called from another dir

--- loader.sh
+++ loader.sh
@@ -1,2 +1,6 @@
-java -cp `find lib -name 'weld-logger-*.jar'`:`find . -name 'loader-*.jar'` \
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+java -cp `find $DIR/lib -name 'weld-logger-*.jar'`:`find $DIR -name 'loader-*.jar'` \
 	com.recomdata.pipeline.entrypoint.Bootstrap $@
