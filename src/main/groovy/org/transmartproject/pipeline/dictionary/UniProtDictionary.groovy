0c9c06545c2b2f5da4d3e65684599953d8d06ece
Peter <peter@thehyve.nl>
2013-11-26 15:16:43 +0100

    Added UniProt dictionary loader.

---
+++ src/main/groovy/org/transmartproject/pipeline/dictionary/UniProtDictionary.groovy
@@ -0,0 +1,11 @@
+package org.transmartproject.pipeline.dictionary
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: peter
+ * Date: 11/26/13
+ * Time: 1:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class UniProtDictionary {
+}
