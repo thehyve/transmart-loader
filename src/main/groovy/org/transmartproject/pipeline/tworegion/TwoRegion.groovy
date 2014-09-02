0547e9af070a835693109c01a1c9a97fda2c8510
Jan Hudecek <j.hudecek@nki.nl>
2014-09-02 17:16:06 +0200

    first version of two region genomic variants ETL

---
+++ src/main/groovy/org/transmartproject/pipeline/tworegion/TwoRegion.groovy
@@ -0,0 +1,228 @@
+package org.transmartproject.pipeline.tworegion
+
+import org.transmartproject.pipeline.util.HighDimImport
+import groovy.sql.BatchingPreparedStatementWrapper
+
+import java.sql.BatchUpdateException
+
+/**
+ * Created by j.hudecek on 7-8-2014.
+ */
+class TwoRegion extends HighDimImport {
+    private static HashMap<String, Integer> genes;
+
+    static main(args) {
+        procedureName = "load_tworegion";
+        initDB()
+
+        //check args, print usage
+        if (!parseOptions(args)) {
+            return
+        };
+
+        cleanupOptions()
+        startAudit()
+        try {
+            insertConceptPath()
+
+            //switch cgs, tophat, soapfuse
+            if (options.cgaJunctions != false) {
+                sampleId = options.sampleId;
+                subjectId = options.subjectId;
+                initSourceSystem(options.sampleId,  options.subjectId)
+
+                insertMetadata()
+
+                insertCgaJunctions()
+                stepCt++;
+
+                insertCgaEvents()
+            }
+            tm_cz.execute("select tm_cz.cz_end_audit ($jobId, 'SUCCESS')");
+        }
+        catch (Exception ex) {
+            handleError(ex)
+        }
+    }
+
+    private static Boolean parseOptions(args) {
+        def cli = new CliBuilder(usage: '-[hscdiatmoxjuf] ',
+                posix:false,
+                header: 'Parameters marked with asterisk * are mandatory')
+        // Create the list of options.
+        cli.with {
+            h longOpt: 'help', 'Show usage information'
+            s longOpt: 'studyId', args: 1, argName: 'studyId', required:true, '* Study ID under which two region data should be imported'
+            c longOpt: 'conceptPath', args: 1, argName: 'conceptCode', required:true, '* Concept path where should the data be stored'
+            d longOpt: 'datasource', args: 1, argName: 'datasource', required:true, '* Identification of the dataset this data comes from'
+            t longOpt: 'tophatFusionPost', args: 1, argName: 'tophatFile', 'Path to the result of tophat fusion-post'
+            o longOpt: 'soapFusion', args: 1, argName: 'soapFile', 'Path to file containing combined SOAPFuse fusion files'
+            x longOpt: 'soapFusionDir', args: 1, argName: 'soapDir', 'Path to directory containing SOAPFuse fusion files with sampleID in their name'
+            m longOpt: 'mapping', args: 1, argName: 'mappingFile', '(TopHat andd SOAPFuse only) Path to the subject sample mapping file (subjectID;sampleID)'
+            j longOpt: 'cgaJunctions', args: 1, argName: 'junctionFile', 'Path to file containing output of CGA tools, junction file, needs to be used with cga-events, sample-id and subject-id'
+            u longOpt: 'cgaEvents', args: 1, argName: 'eventsFile', 'Path to file containing output of CGA tools, events file, needs to be used with cga-junctions, sample-id and subject-id'
+            i longOpt: 'subjectId', args: 1, argName: 'subjectId', '(for CGA only) ID of the subject to which this sample belongs to'
+            a longOpt: 'sampleId', args: 1, argName: 'sampleId', '(for CGA only) ID of the sample'
+            f longOpt: 'force',   'Answer yes to all prompts'
+            r longOpt: 'ref', args: 1, argName: 'ref', 'Human genome reference version (i.e. hg18)'
+        }
+
+        options = cli.parse(args)
+        // Show usage text when -h or --help option is used.
+        if (!options)
+            return false;
+        if  (options.h) {
+        } else if (options.t && !options.m){
+            println('for tophat a mapping file is needed');
+        } else if ((options.o || options.x) && !options.m){
+            println('for SOAPFuse a mapping file is needed');
+        } else if ((options.j == false) != (options.u == false)){
+            println('for CGA both the junction and events file is needed');
+        } else if (options.j && options.u && (!options.i ||  !options.a)){
+            println('for CGA both the sample id and subject id has to be specified');
+        } else if ((!options.t || !options.m) && ((!options.o && !options.x) || !options.m) && (!options.j || !options.u)){
+            println('one of tophat, cga or soapfuse input files must be specified');
+        } else if (options.r && (options.r != 'hg17' || options.r != 'hg18' || options.r != 'hg19')) {
+            println('unknown reference');
+        } else {
+            if (options.t && !new File(options.t).exists()) {
+                println('tophat fusion post doesn\'t exist!');
+                return false;
+            }
+            if (options.m && !new File(options.m).exists()) {
+                println('mapping file doesn\'t exist!');
+                return false;
+            }
+            if (options.o && !new File(options.o).exists()) {
+                println('SOAPFuse file doesn\'t exist!');
+                return false;
+            }
+            if (options.x && !new File(options.x).exists()) {
+                println('SOAPFuse directory doesn\'t exist!');
+                return false;
+            }
+            if (options.j && !new File(options.j).exists()) {
+                println('CGA junction file doesn\'t exist!');
+                return false;
+            }
+            if (options.u && !new File(options.u).exists()) {
+                println('CGA events file doesn\'t exist!');
+                return false;
+            }
+            return true;
+        }
+        cli.usage();
+        return false;
+    }
+
+    private static void insertCgaJunctions() {
+        String platform;
+        try {
+            def updatedCounts = deapp.withBatch(100, "INSERT INTO deapp.de_two_region_junction\
+             (up_chr, up_pos, up_strand, up_len, down_chr, down_pos, down_strand, down_len, is_in_frame, external_id, assay_id) VALUES \
+            (:up_chr,:up_pos,:up_strand,:up_len,:down_chr,:down_pos,:down_strand,:down_len, null,       :external_id,:assay_id)", {
+                BatchingPreparedStatementWrapper it ->
+                    new File(options.cgaJunctions).eachLine {line ->
+                        if (line[0] == '#' || line[0] == ' ' || line[0] == '>') {
+                            //header
+                            if (line.startsWith('#GENOME_REFERENCE\t')) {
+                                platform = line.replaceFirst('#GENOME_REFERENCE\t', '')
+                            }
+                            return;
+                        }
+                        def tokens = line.split();
+                        // Id            LeftChr LeftPosition       LeftStrand          LeftLength         RightChr              RightPosition     RightStrand                RightLength
+
+                        it.addBatch([
+                                'id'         : Integer.parseInt(tokens[0]),
+                                'up_chr'     : tokens[1],
+                                'up_pos'     : Integer.parseInt(tokens[2]),
+                                'up_strand'  : tokens[3],
+                                'up_len'     : Integer.parseInt(tokens[4]),
+                                'down_chr'   : tokens[5],
+                                'down_pos'   : Integer.parseInt(tokens[6]),
+                                'down_strand': tokens[7],
+                                'down_len'   : Integer.parseInt(tokens[8]),
+                                'assay_id'   : assayId])
+                    }
+            })
+            def totalUpdated = (updatedCounts as Integer[]).sum();
+            tm_cz.execute("select tm_cz.cz_write_audit($jobId,'TM_CZ',$procedureName,'Inserted cga junctions',$totalUpdated,$stepCt,'Done')");
+        }
+        catch (BatchUpdateException ex) {
+            throw ex.getNextException() //real exception is in the next one, container exception is just "batch failed"
+        }
+    }
+
+    private static void insertCgaEvents() {
+        //create a map of external id - id
+        Map<Integer, Integer> internalIds = deapp.rows("select external_id, two_region_junction_id from deapp.de_two_region_junction where assay_id=$assayId").collectEntries {
+            [(it[0]): it[1]]
+        }
+
+        Integer eventsCount = 0, junctionEventsCount = 0, genesCount = 0;
+        new File(options.cgaEvents).eachLine {line ->
+            if (line.length() == 0 || line[0] == '#' || line[0] == ' ' || line[0] == '>') {
+                //header
+                return;
+            }
+
+            def tokens = line.split();
+            //EventId	Type	RelatedJunctionIds	MatePairCounts	FrequenciesInBaselineGenomeSet	OriginRegionChr	OriginRegionBegin	OriginRegionEnd	OriginRegionLength	OriginRegionStrand	DestinationRegionChr	DestinationRegionBegin	DestinationRegionEnd	DestinationRegionLength	DestinationRegionStrand	DisruptedGenes	ContainedGenes	GeneFusions	RelatedMobileElement	MobileElementChr	MobileElementBegin	MobileElementEnd	MobileElementStrand
+
+            GString query = "INSERT INTO deapp.de_two_region_event( \
+                                         cga_type, soap_class) \
+                                 VALUES (${tokens[1]}, null);"
+            deapp.execute(query);
+            eventsCount++;
+            if (eventsCount % 100 == 0) {
+                print('.')
+            }
+
+            def junctions = tokens[2].split(';');
+            def matePairCounts = tokens[3].split(';');
+            def baseFreqs = tokens[4].split(';');
+            for (int i = 0; i < junctions.length; i++) {
+                def junction = Integer.parseInt(junctions[i]);
+                def matePairCount = Integer.parseInt(matePairCounts[i]);
+                def baseFreq = Double.parseDouble(baseFreqs[i]);
+                query = "INSERT INTO deapp.de_two_region_junction_event( \
+                                             junction_id, event_id,  \
+                                             reads_span, reads_junction, pairs_span, pairs_junction,  \
+                                             pairs_end, reads_counter, base_freq) \
+                                     VALUES (${internalIds[junction]}, currval( 'de_two_region_event_seq'), null, null, $matePairCount, null, null, null, $baseFreq );"
+                deapp.execute(query);
+                junctionEventsCount++;
+            }
+
+            /* TODO: integrate with genes
+            def disruptedGgenes = tokens[15].split(';').findAll({ !it.matches('^LOC[0-9]+')});
+            for (int i=0;i<disruptedGgenes.length;i++) {
+
+                query = "INSERT INTO deapp.de_two_region_event_gene( \
+                gene_id, event_id, effect) VALUES   \
+               (?, ?, ?);
+                "
+                deapp.execute(query);
+                genesCount++;
+            }
+            //def containedGenes = tokens[16].split(';').findAll({ !it.matches('^LOC[0-9]+')});
+            //def fusionGenes = tokens[17].split(';');
+            */
+
+        }
+        tm_cz.execute("select tm_cz.cz_write_audit($jobId,'TM_CZ',$procedureName,'Inserted cga events',$eventsCount,$stepCt,'Done')");
+        tm_cz.execute("select tm_cz.cz_write_audit($jobId,'TM_CZ',$procedureName,'Inserted cga junction-event links',$junctionEventsCount,$stepCt,'Done')");
+        tm_cz.execute("select tm_cz.cz_write_audit($jobId,'TM_CZ',$procedureName,'Inserted cga gene-event links',$genesCount,$stepCt,'Done')");
+        println('')
+        println("Successfully inserted $eventsCount events")
+    }
+
+
+    private static void getBiomarkerId(String hugo) {
+        String query =     ''
+        //TODO: get entrez id from hugo
+    }
+
+
+}
---
+++ src/main/groovy/org/transmartproject/pipeline/util/HighDimImport.groovy
@@ -0,0 +1,285 @@
+package org.transmartproject.pipeline.util
+
+import groovy.sql.Sql
+import org.apache.log4j.Logger
+import org.apache.log4j.PropertyConfigurator
+import org.transmartproject.pipeline.tworegion.TwoRegion
+
+import java.sql.SQLException
+
+/**
+ * Created by j.hudecek on 2-9-2014.
+ */
+class HighDimImport {
+    protected static final Logger log = Logger.getLogger(TwoRegion)
+    protected static OptionAccessor options
+
+    //DBs
+    protected static Sql deapp
+    protected static Sql i2b2demodata
+    protected static Sql i2b2metadata
+    protected static Sql tm_lz
+    protected static Sql tm_cz
+
+    //per run vars
+    protected static String datasetId
+    protected static String sampleId
+    protected static String concept
+    protected static String subjectId
+
+    //processed vars
+    protected static String sourceSystem
+    protected static String procedureName
+    protected static int stepCt
+
+    //DB IDs
+    protected static int jobId
+    private static Integer conceptCd
+    private static BigDecimal patientNum
+    protected static Integer assayId
+
+    protected static void initDB() {
+        PropertyConfigurator.configure("conf/log4j.properties");
+
+        log.info("Start loading property file Common.properties ...")
+        Properties props = Util.loadConfiguration("conf/Common.properties");
+
+        deapp = Util.createSqlFromPropertyFile(props, "deapp")
+        i2b2demodata = Util.createSqlFromPropertyFile(props, "i2b2demodata")
+        i2b2metadata = Util.createSqlFromPropertyFile(props, "i2b2metadata")
+        tm_lz = Util.createSqlFromPropertyFile(props, "tm_lz")
+        tm_cz = Util.createSqlFromPropertyFile(props, "tm_cz")
+    }
+
+    protected static void cleanupOptions() {
+        datasetId = options.datasetId;
+        concept = options.conceptPath
+
+        datasetId = datasetId.replace("'", "''");
+        concept = concept.replace("'", "''");
+
+        concept = concept.replace('/', '\\').replace('_', ' ').replace('%', 'Pct').replace('&', ' and ').replace('+', ' and ');
+        if (!concept.startsWith('\\')) {
+            concept = '\\' + concept
+        };
+        if (!concept.endsWith('\\')) {
+            concept += '\\'
+        };
+    }
+
+    protected static void startAudit() {
+        //Audit JOB Initialization
+        jobId = tm_cz.firstRow("select tm_cz.cz_start_audit ($procedureName, 'TM_CZ')")[0];
+        tm_cz.execute("select tm_cz.cz_write_audit($jobId,'TM_CZ',$procedureName,'Starting metadata creation',0,1,'Done')");
+    }
+
+    protected static void insertMetadata() {
+        insertPatient()
+        insertSubjectSampleMapping()
+        insertObservationFact()
+        insertSampleDimension()
+    }
+
+    protected static void insertConceptPath() {
+        Integer topLevel = concept.length() - concept.replace('\\', '').length();
+        if (topLevel < 3) {
+            throw new IllegalArgumentException("Path specified in top_node must contain at least 2 nodes");
+        }
+        def pathEls = concept.split('\\\\');
+        String path = "\\"
+        for (Integer level = 1; level < topLevel; level++) {
+            String node = pathEls[level];
+            path = path + node + "\\";
+            String attr = (level == topLevel - 1) ? "LAH" : "FA";
+            conceptCd = insertConceptNode(path, node, level, attr)
+        }
+    }
+
+    protected static void initSourceSystem(String sampleId, String subjectId) {
+        sampleId = sampleId.replace("'", "''");
+        subjectId = subjectId.replace("'", "''");
+
+        sourceSystem = "${datasetId}:${sampleId}";
+    }
+
+    protected static void handleError(Exception ex) {
+        ex.printStackTrace()
+        tm_cz.execute("select tm_cz.cz_write_error($jobId, '1', ${ex.message}, ${ex.toString()} ,'')");
+        tm_cz.execute("select tm_cz.cz_end_audit ($jobId, 'FAIL')");
+    }
+
+    private static void insertSampleDimension() {
+        def inserted = i2b2demodata.executeInsert("INSERT INTO i2b2demodata.sample_dimension(sample_cd) \
+                                                                 SELECT $sampleId  where NOT EXISTS(SELECT NULL FROM i2b2demodata.sample_dimension WHERE sample_cd=$sampleId );");
+        tm_cz.execute("select tm_cz.cz_write_audit($jobId ,'TM_CZ',$procedureName ,'Inserted sample_dimension, metadata done',${inserted.size()},$stepCt ,'Done')");
+        stepCt++;
+    }
+
+    private static void insertObservationFact() {
+        i2b2demodata.execute("insert into i2b2demodata.observation_fact \
+                             (encounter_num \
+                                 ,patient_num \
+                                 ,concept_cd \
+                                 ,start_date \
+                                 ,modifier_cd \
+                                 ,valtype_cd \
+                                 ,tval_char \
+                                 ,sourcesystem_cd \
+                                 ,import_date \
+                                 ,valueflag_cd \
+                                 ,provider_id \
+                                 ,location_cd \
+                                 ,units_cd \
+                                 ,instance_num \
+                             ) \
+                             select distinct $patientNum \
+                                           ,$patientNum \
+                                           ,$conceptCd \
+                                           ,current_timestamp \
+                                           ,'@' \
+      /*Text data type*/                   ,'T' \
+      /* Stands for Equals for Text Types*/,'E'  \
+                                           ,$datasetId \
+                                           ,current_timestamp \
+                                           ,'@' \
+                                           ,'@' \
+                                           ,'@' \
+                                           ,'' \
+                                           , 0");
+        tm_cz.execute("select tm_cz.cz_write_audit($jobId ,'TM_CZ',$procedureName ,'Inserted observation_fact',1,$stepCt ,'Done')");
+        stepCt++;
+    }
+
+    private static void insertSubjectSampleMapping() {
+        //first try to insert the mapping, if it exists get its num. We assume it usually won't exist
+        def inserted = deapp.executeInsert("insert into deapp.de_subject_sample_mapping (\
+                                    patient_id, \
+                                    subject_id, \
+                                    assay_id, \
+                                    concept_code, \
+                                    trial_name, \
+                                    platform) \
+                                                         select $patientNum, \
+                                                         $sampleId, \
+                                                         nextval( 'deapp.seq_assay_id' ), \
+                                                         $conceptCd, \
+                                                         $datasetId, \
+                                                         'two_region'\
+                                            WHERE NOT EXISTS ( SELECT NULL FROM deapp.de_subject_sample_mapping WHERE concept_code = $conceptCd  AND subject_id=$sampleId  );\n");
+        if (inserted.empty) {
+            assayId = i2b2demodata.firstRow("SELECT assay_id FROM deapp.de_subject_sample_mapping WHERE concept_code = $conceptCd  AND subject_id=$sampleId ")[0];
+        } else {
+            assayId = inserted[0][0];
+            //I'm not sure why it is on the second position, better check we really have the right patient_num:
+            if (null == i2b2demodata.firstRow("SELECT assay_id FROM deapp.de_subject_sample_mapping WHERE concept_code = $conceptCd  AND subject_id=$sampleId  AND assay_id=$assayId ")) {
+                throw new SQLException("Getting assay_id failed")
+            };
+            stepCt++;
+            tm_cz.execute("select tm_cz.cz_write_audit($jobId ,'TM_CZ',$procedureName ,'Inserted de_subject_sample_mapping',1,$stepCt ,'Done')");
+        }
+    }
+
+    private static void insertPatient() {
+        //first try to insert the patient, if it exists get its num. We assume it usually won't exist
+        def inserted = i2b2demodata.executeInsert("insert into i2b2demodata.patient_dimension \
+                                                 (patient_num, \
+                                                         sex_cd, \
+                                                         age_in_years_num, \
+                                                         race_cd, \
+                                                         update_date, \
+                                                         download_date, \
+                                                         import_date, \
+                                                         sourcesystem_cd \
+                                                 ) \
+                                           select nextval('i2b2demodata.seq_patient_num'), \
+                                                         'Unknown', \
+                                                         0, \
+                                                         null, \
+                                                         current_timestamp, \
+                                                         current_timestamp, \
+                                                         current_timestamp, \
+                                                         $sourceSystem\
+                                            WHERE NOT EXISTS ( SELECT NULL FROM i2b2demodata.patient_dimension WHERE sourcesystem_cd = $sourceSystem  );\n");
+        if (inserted.empty) {
+            patientNum = new BigDecimal(i2b2demodata.firstRow("SELECT patient_num FROM i2b2demodata.patient_dimension WHERE sourcesystem_cd = $sourceSystem ")[0]);
+        } else {
+            patientNum = new BigDecimal(inserted[0][0]);
+            //I'm not sure why it is on the second position, better check we really have the right patient_num:
+            if (null == i2b2demodata.firstRow("SELECT patient_num FROM i2b2demodata.patient_dimension WHERE sourcesystem_cd = $sourceSystem  AND patient_num=$patientNum ")) {
+                throw new SQLException("Getting patient_num failed")
+            };
+            stepCt++;
+            tm_cz.execute("select tm_cz.cz_write_audit($jobId ,'TM_CZ',$procedureName ,${'Added patient '+sourceSystem},0,$stepCt ,'Done')");
+        }
+    }
+
+    private static int insertConceptNode(String path, String node, int level, String attr) {
+        Integer conceptId;
+        //don't insert root node ("Public Studies") with a specific dataset id
+        String sourcesystem = level == 1 ? null : datasetId;
+
+        //first try to insert concept, if it exists get its cd. We assume it usually won't exist
+        def inserted = i2b2demodata.executeInsert("insert into i2b2demodata.concept_dimension (concept_cd, concept_path, name_char, update_date, download_date, import_date, sourcesystem_cd)\
+                   SELECT nextval( 'i2b2demodata.concept_id' ),$path,$node,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,null  \
+                   WHERE NOT EXISTS ( SELECT NULL FROM i2b2demodata.concept_dimension WHERE concept_path = $path );\n");
+        if (inserted.empty) {
+            conceptId = Integer.parseInt(i2b2demodata.firstRow("SELECT concept_cd FROM i2b2demodata.concept_dimension WHERE concept_path = $path")[0]);
+        } else {
+            conceptId = Integer.parseInt(inserted[0][1]);
+            //I'm not sure why it is on the second position, better check we really have the right concept_cd:
+            if (null == i2b2demodata.firstRow("SELECT concept_cd FROM i2b2demodata.concept_dimension WHERE concept_path = $path AND concept_cd=$conceptId")) {
+                throw new SQLException("Getting concept_cd failed")
+            };
+            stepCt++;
+            tm_cz.execute("select tm_cz.cz_write_audit($jobId ,'TM_CZ',$procedureName ,${'Added concept '+path},0,$stepCt ,'Done')");
+        }
+
+        if (!i2b2metadata.executeInsert("insert into i2b2metadata.i2b2 \
+                                                 (c_hlevel \
+                                                  ,c_fullname \
+                                                  ,c_name \
+                                                  ,c_visualattributes \
+                                                  ,c_synonym_cd \
+                                                  ,c_facttablecolumn \
+                                                  ,c_tablename \
+                                                  ,c_columnname \
+                                                  ,c_dimcode \
+                                                  ,c_tooltip \
+                                                  ,update_date \
+                                                  ,download_date \
+                                                  ,import_date \
+                                                  ,sourcesystem_cd \
+                                                  ,c_basecode \
+                                                  ,c_operator \
+                                                  ,c_columndatatype \
+                                                  ,c_comment \
+                                                  ,m_applied_path \
+                                                  ,c_metadataxml \
+                                                 ) \
+                                 select $level \
+                                         ,$path \
+                                         ,$node \
+                                         ,$attr \
+                                         ,'N' \
+                                         ,'CONCEPT_CD' \
+                                         ,'CONCEPT_DIMENSION' \
+                                         ,'CONCEPT_PATH' \
+                                         ,$path \
+                                         ,$path \
+                                         ,current_timestamp \
+                                         ,current_timestamp \
+                                         ,current_timestamp \
+                                         ,$sourcesystem \
+                                         ,$conceptId \
+                                         ,'LIKE' \
+                                         ,'T' \
+                                         ,$sourceSystem \
+                                         ,'@' \
+                                         ,null\
+                                WHERE NOT EXISTS ( SELECT NULL FROM i2b2metadata.i2b2 WHERE c_fullname = $path )").empty) {
+            stepCt++;
+            tm_cz.execute("select tm_cz.cz_write_audit($jobId ,'TM_CZ',$procedureName ,${'Added i2b2 '+path},0,$stepCt ,'Done')");
+        }
+        return conceptId;
+    }
+}
